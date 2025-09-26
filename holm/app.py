42c1b8b520315e807e06cd5c4d2e4cb019240a04
Peter Volf <do.volfp@gmail.com>
2025-09-26 15:34:13 +0200

    fix: use Path.parts instead of string splitting, refs #5

--- holm/app.py
+++ holm/app.py
@@ -140,7 +140,7 @@ def _discover_app_packages(config: AppConfig) -> set[PackageInfo]:
         rel_path = path.relative_to(config.root_dir)
         return any(
             p
-            for p in str(rel_path).split("/")
+            for p in rel_path.parts
             # Exclude if a path segment starts with an underscore but does not end with one.
             # Path segments that both start and end with an underscore represent path parameters!
             if p.startswith("_") and not p.endswith("_")
