8e6b6161f699038372b54082eac1f750e5efddca
Peter Volf <do.volfp@gmail.com>
2025-10-25 16:25:20 +0200

    docs: add documentation for without_layout and bump minor version, refs #27

--- holm/__init__.py
+++ holm/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 
 from .app import App as App
 from .fastapi import FastAPIDependency as FastAPIDependency
 from .fastapi import FastAPIErrorHandler as FastAPIErrorHandler
 from .module_options._metadata import Metadata as Metadata
 from .modules._error import ErrorHandlerMapping as ErrorHandlerMapping
+from .modules._layout import without_layout as without_layout
